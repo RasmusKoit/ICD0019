01d00247cc896bc1de942537d7bc7a28ee440d19
Rasmus Koit <rasmuskoit@gmail.com>
2018-03-08 11:27:53 +0200

    FileEdits

---
+++ fileEdits/src/Ymber.java
@@ -0,0 +1,51 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ymber {
+
+    static void perm(String algus, String a, boolean valik) {
+        if (a.length() == 1 && valik)
+            System.out.println(algus + a.substring(0, 1));
+            for (int i = 0; i < a.length(); i++)
+                perm(algus + a.substring(i, i + 1),
+                        a.substring(0, i) + a.substring(i + 1), valik);
+    }
+    static void replaceWord(String sona, String millest, String milleks) {
+        System.out.println(sona.replace(millest, milleks));
+        String failinimi = "replaced.txt";
+        Scanner s = new Scanner(System.in);
+        System.out.println("Sisesta midagi muud: ");
+        String teineSona = s.nextLine();
+        if (!teineSona.isEmpty()) {
+            sona = teineSona;
+        }
+
+        try {
+            FileWriter fw = new FileWriter(failinimi);
+            fw.write(sona.replace(millest, milleks));
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("ei saanud");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+//        String b = "ATIAAABD";
+//        String c = "ATIAAABDAA";
+//        long start=System.currentTimeMillis();
+//        perm(" ", b, true);
+//        long stop=System.currentTimeMillis();
+//        System.out.println("Aega läks " + (stop - start) + "ms" + " Stringi pikkusega " + b.length());
+//        start=System.currentTimeMillis();
+//        perm(" ", c, false);
+//        stop=System.currentTimeMillis();
+//        System.out.println("Aega läks " + (stop - start) + "ms" + " Stringi pikkusega " + c.length());
+        String sona = args[0];
+        replaceWord(sona, "x", "ks");
+
+    }
+}
